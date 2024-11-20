0651d4bea40d0e439ddfe6b933dad67e386c253f
Pravintobi <a.pravin1922@gmail.com>
2024-11-20 21:03:55 +0530

    dd

--- Capstone/src/User/route/admin.js
+++
@@ -1,9 +0,0 @@
-// import Express from 'express'
-// import admin from '../controller/admin.js'
-// const router = Express.Router()
-
-// router.post('/examquestion',admin.createTrueFalseQuestion)
-
-
-
-// export default router
